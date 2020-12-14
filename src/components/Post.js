1df919fc598357519f75836ede2c67757c4bc1f9
my-name <>
2020-12-14 22:33:25 +0100

    Added styling to posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client";
+import { Breakpoint } from "../shared";
+import styled from "styled-components";
+const PostContainer = styled.div`
+  margin: 10px 20px;
+  display: grid;
+  grid-auto-rows: 200px 200px;
+  grid-gap: 5px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-auto-flow: dense;
+  ${Breakpoint.LaptopOrLarger} {
+    grid-auto-rows: 300px 300px;
+    grid-gap: 10px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+`;
+
+const PostItem = styled.article`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  position: relative;
+
+  img {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: 50% 50%;
+    cursor: pointer;
+    transition: 0.5s ease-in-out;
+  }
+  h2 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: red;
+    z-index: 2;
+    margin: 0;
+  }
+`;
 
 export const Post = () => {
   const [postData, setPost] = useState(null);
   const query = `*[_type == "post"]{
       slug, 
-      title, 
+      title,
+      link, 
       mainImage{
         asset->{
           _id, 
@@ -23,19 +66,19 @@ export const Post = () => {
       .catch(console.error);
   });
   return (
-    <div>
+    <PostContainer>
       {postData &&
         postData.map((post, index) => (
-          <article key={index}>
-            <h1>{post.title}</h1>
+          <PostItem key={index}>
             <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+              <h2>{post.title}</h2>
               <img
                 src={post.mainImage.asset.url}
                 alt={post.mainImage.asset.alt}
               />
             </Link>
-          </article>
+          </PostItem>
         ))}
-    </div>
+    </PostContainer>
   );
 };
--- src/shared.js
+++ src/shared.js
@@ -1,7 +1,14 @@
-export const Breakpoints = {
-    TabletOrLarger = '@media (min-width: 501px)',
-  LaptopOrLarger = '@media (min-width: 813px)',
-  MediumScreenOrLarger = '@media (min-width: 990px)',
-  BigScreenOrLarger = '@media (min-width: 1246px)',
-  TabletOrSmaller = '@media (max-width: 812px)',
-}
+export const Breakpoint = {
+  OldPhone: "@media (max-width: 320px)",
+  Phone: "@media (max-width: 500px)",
+  IosTablet: "@supports (-webkit-overflow-scrolling: touch)",
+  PhonePortait:
+    "@media only screen and (orientation: portrait) and (max-width: 500px)",
+  Tablet: "@media (min-width: 501px) and (max-width: 812px)",
+  Laptop: "@media (min-width: 813px) and (max-width: 1100px)",
+  TabletOrLarger: "@media (min-width: 501px)",
+  LaptopOrLarger: "@media (min-width: 813px)",
+  MediumScreenOrLarger: "@media (min-width: 990px)",
+  BigScreenOrLarger: "@media (min-width: 1246px)",
+  TabletOrSmaller: "@media (max-width: 812px)",
+};
